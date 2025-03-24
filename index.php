978e91f65dbd383c941ee43087a664768b4dc1ac
0DanielMelo <danielmelo0@hotmail.com>
2025-03-24 20:40:25 -0300

    .

--- index.php
+++ index.php
@@ -6,6 +6,18 @@
     <title>Document</title>
 </head>
 <body>
+
+<?php
+    $valor = 1;
+    $valor1 = 1;
+
+    if ($valor>0 && $valor1 > 0){
+
+        echo 'Ambos os Valores são Maiores que 0';
+    }else {
+        echo 'The Condition Return False';
+    }
+?>
     
 </body>
 </html>
