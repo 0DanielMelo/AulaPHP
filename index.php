1273b0f55eff42f0542e9c664c7216e13d4f1fea
Daniel Melo <danielmelo0@hotmail.com>
2025-03-25 19:24:58 -0300

    Feat : Add Files Via Upload
    .

--- index.php
+++ index.php
@@ -1,23 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+<?php 
 
-<?php
-    $valor = 1;
-    $valor1 = 1;
+include "header.php";
+include "form.php";
+include 'content.php';
 
-    if ($valor>0 && $valor1 > 0){
-
-        echo 'Ambos os Valores são Maiores que 0';
-    }else {
-        echo 'The Condition Return False';
-    }
-?>
     
-</body>
-</html>
+?>
